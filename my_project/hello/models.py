461e6a2baba44c419ab7e093fcd2fb899138961d
Elandr <abugaew53@gmail.com>
2026-02-23 21:13:34 +0300

    changed database from sqlite to postgresql

--- my_project/hello/models.py
+++ my_project/hello/models.py
@@ -5,3 +5,7 @@ class Person(models.Model):
     age = models.IntegerField()
     email = models.CharField(max_length=40)
     surname = models.CharField(max_length=20)
+
+    def __str__(self):
+        """some sting for the future events"""
+        return self.name
--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
             ],
         },
     },
-]
+]   
 
 WSGI_APPLICATION = 'my_project.wsgi.application'
 
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres-test',
+        'USER': 'postgres',
+        'PASSWORD': '2299722',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
